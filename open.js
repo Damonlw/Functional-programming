bd5a77e779cc7cefa734ae1f29c30e9e88fd05a5
Damon <Damon@MBP.local>
2017-03-09 15:14:14 +0800

    modify

--- open.js
+++ open.js
@@ -1,13 +1,16 @@
 /**
  * Show Time
  */
+//函数组合
+
 const compose = function (f, g) {
 	return function (x) {
 		return f(g(x))
 	}
 }
+
 const head = function (x) { return x[0] }
 const exclainm = function (x) { return x + '!' }
 const result = compose(head, exclainm)
 
-console.log(last(['jumpkick', 'roundhouse', 'uppercut']))
+console.log(result(['jumpkick', 'roundhouse', 'uppercut']))
