2a200abaeee921140008399d75ae0c71fffd8fea
Damon <Damon@MBP.local>
2017-03-09 14:29:19 +0800

    compose

--- open.js
+++ open.js
@@ -1,3 +1,13 @@
 /**
  * Show Time
  */
+const compose = function (f, g) {
+	return function (x) {
+		return f(g(x))
+	}
+}
+const head = function (x) { return x[0] }
+const exclainm = function (x) { return x + '!' }
+const result = compose(head, exclainm)
+
+console.log(last(['jumpkick', 'roundhouse', 'uppercut']))
