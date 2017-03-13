e4b9c80282d71b993e6da2143a87359177c5aadb
Damon <Damon@MBP.local>
2017-03-13 22:26:53 +0800

    add new

--- open.js
+++ open.js
@@ -3,14 +3,27 @@
  */
 //函数组合
 
-const compose = function (f, g) {
-	return function (x) {
+const compose = function(f, g) {
+	return function(x) {
 		return f(g(x))
 	}
 }
 
-const head = function (x) { return x[0] }
-const exclainm = function (x) { return x + '!' }
+const head = function(x) { return x[0] }
+const exclainm = function(x) { return x + '!' }
 const result = compose(head, exclainm)
 
 console.log(result(['jumpkick', 'roundhouse', 'uppercut']))
+
+
+class Func {
+	constructor(val) {
+		this.val = val
+	}
+
+	Map(f) {
+		return new Func(f(this.val))
+	}
+}
+
+const fn = new Func()
