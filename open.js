18f1d34405802b83c947d9ee63c73bf1b4a0708b
Damon <Damon@MBP.local>
2017-03-14 14:38:49 +0800

    motation

--- open.js
+++ open.js
@@ -9,11 +9,11 @@ const compose = function(f, g) {
 	}
 }
 
-const head = function(x) { return x[0] }
-const exclainm = function(x) { return x + '!' }
-const result = compose(head, exclainm)
-
-console.log(result(['jumpkick', 'roundhouse', 'uppercut']))
+// const head = function(x) { return x[0] }
+// const exclainm = function(x) { return x + '!' }
+// const result = compose(head, exclainm)
+//
+// console.log(result(['jumpkick', 'roundhouse', 'uppercut']))
 
 
 
@@ -30,13 +30,35 @@ console.log(result(['jumpkick', 'roundhouse', 'uppercut']))
 // 	}
 // }
 //
-// (new Func(2)).Map((x) => x + 2) // Func { val: 4 }
+// (new Func(2)).Map(x => x + 2) // Func { val: 4 }
 
 
 //函数式编程
 
-Func.of = function(val) {
-	return new Func(val)
-}
+// Func.of = function(val) {
+// 	return new Func(val)
+// }
+//
+// Func.of(2).map(x => x + 2)
 
-Func.of(2).map((x) => x + 2)
+// const origin = [1, 2, 3]
+
+// // 命令式
+// const makes = [] // [1, 2, 3]
+// for (i = 0; i < origin.length; i++) {
+// 	makes.push(origin[i])
+// }
+
+// // 表达式
+// const makes = origin.map(x => x) // [1, 2, 3]
+//
+// // || 刷副本，打怪升级...K.O
+// // \/
+//
+// const makes = function(arr) {
+// 	return arr.map(x => x)  // [1, 2, 3]
+// }
+// // /\
+// // || 自由切换
+// // \/
+// const makes = arr => arr.map(x => x)
